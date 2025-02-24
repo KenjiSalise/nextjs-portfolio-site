c95e6a1b62314c8f6bbe42a01884fcfc97a1ae90
Kenji Sebastian Salise <kenjimsalise@gmail.com>
2025-02-24 16:14:02 +0900

    fixed project table

--- src/components/ProjectTable.tsx
+++ src/components/ProjectTable.tsx
@@ -9,6 +9,7 @@ interface Item{
     image:string
     alt:string
     desc:string
+
 }
 interface DataType {
     data: Item[];
@@ -20,9 +21,9 @@ const ProjectTable = () => {
   return (
     <main>
         <div className='grid grid-cols-2 gap-10 justify-center flex mx-10 my-5'>
-        {data.map((item:Item)=>(
+        {data.map((item:Item, index)=>(
           <Link href={item.link}>
-           <div className='box-border h-[10rem] transition-all duration-150 ease-in-out hover:shadow-[#3a2f2f] hover:bg-[#3a2f2f] width-10 rounded-lg px-5 bg-[#453A4C] ring-[#362c3d] shadow-[#362c3d] shadow-2xl inset-shadow-[#453A4C]'>
+           <div key={index} className='box-border h-[10rem] transition-all duration-150 ease-in-out hover:shadow-[#3a2f2f] hover:bg-[#3a2f2f] width-10 rounded-lg px-5 bg-[#453A4C] ring-[#362c3d] shadow-[#362c3d] shadow-2xl inset-shadow-[#453A4C]'>
             <h1 className='text-[3rem]'>{item.name}</h1>
             <p className='text-[1rem]'>{item.desc}</p>
            </div>

a5d3cd9f1255c12b7fc0b9121a27330a29eb755e
Kenji Sebastian Salise <kenjimsalise@gmail.com>
2025-02-25 15:33:43 +0900

    fix ProjectTable

--- src/components/ProjectTable.tsx
+++ src/components/ProjectTable.tsx
@@ -3,33 +3,38 @@ import Data from '../../public/test.json'
 import Link from 'next/link'
 
 
-interface Item{
-    name:string
-    link:string
-    image:string
-    alt:string
-    desc:string
+interface Item {
+  name: string
+  link: string
+  image: string
+  alt: string
+  desc: string
 
 }
 interface DataType {
-    data: Item[];
-  }
-  
+  data: Item[];
+}
+
 const ProjectTable = () => {
-    const { data } = Data as DataType;
-  
+  const { data } = Data as DataType;
+
   return (
     <main>
-        <div className='grid grid-cols-2 gap-10 justify-center flex mx-10 my-5'>
-        {data.map((item:Item, index)=>(
-          <Link key={index} href={item.link}>
-           <div className='box-border h-[10rem] transition-all duration-150 ease-in-out hover:shadow-[#3a2f2f] hover:bg-[#3a2f2f] width-10 rounded-lg px-5 bg-[#453A4C] ring-[#362c3d] shadow-[#362c3d] shadow-2xl inset-shadow-[#453A4C]'>
+      <div className="grid grid-cols-2 gap-8 justify-center mx-10 my-5">
+        {data.map((item: Item, index) => (
+          <Link href={item.link} key={index} className='
+            rounded-md hover:rounded-3xl
+            p-4
+            bg-[#453A4C] hover:bg-[#3a2f2f]
+            ring ring-[#362c3d]
+            transition-all duration-150 ease-in-out
+            shadow-2xl inset-shadow-[#453A4C] shadow-[#362c3d] hover:shadow-[#3a2f2f]
+          '>
             <h1 className='text-[3rem]'>{item.name}</h1>
             <p className='text-[1rem]'>{item.desc}</p>
-           </div>
-           </Link>
+          </Link>
         ))}
-        </div>
+      </div>
     </main>
   )
 }
